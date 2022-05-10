30de454bb98d996ac6873f6df2be53e02baf3df3
Paul Gottschling <paul.gottschling@gmail.com>
2022-05-09 21:59:05 -0400

    Start adding the main loop

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/go-yaml/yaml"
 )
 
 type Config struct {
@@ -19,6 +26,19 @@ type OneDriveConfig struct {
 	ClientSecret  string `json:"client_secret"`
 	DirectoryPath string `json:"directory_path"`
 }
+
+const todoistBackupURL = "https://api.todoist.com/sync/v8/backups/get"
+
+type AvailableBackups []AvailableBackup
+
+type AvailableBackup struct {
+	Version string `json:"version"`
+	URL     string `json:"url"`
+}
+
+// The OneDrive simple upload API supports uploads of up to 4MB.
+// https://docs.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_put_content
+const oneDriveMaxBytes = 4e6
 
 const help = `You must provide a -config flag with the path to a config file.
 
@@ -39,11 +59,12 @@ onedrive:
 	The Todoist backup job will be limited to this directory.
 
 backup_interval: How often to conduct the backup. A duration string like 1m, 
-4h, or 3d.
-
-`
+4h, or 3d.`
 
 func main() {
+	var g chan os.Signal
+	signal.Notify(g, os.Interrupt)
+
 	cf := flag.String("config", "", "the path to a configuration file")
 	flag.Parse()
 
@@ -59,11 +80,84 @@ func main() {
 		os.Exit(1)
 	}
 
-	// TODO: Decode the YAML into a Config
+	var c Config
+
+	if err := yaml.NewDecoder(f).Decode(&c); err != nil {
+		fmt.Fprintln(os.Stderr, "Could not parse your config file: "+err.Error())
+		os.Exit(1)
+	}
+
 	// TODO: Validate the config
-	// TODO: in a loop:
-	// TODO: Authenticate with Microsoft Graph/Azure AD
-	// TODO: Get a list of backups to download from Todoist
-	// TODO: Grab a payload from Todoist
-	// TODO: Send the payload to Microsoft Graph
+
+	cred, err := azidentity.NewClientSecretCredential(
+		c.OneDrive.TenantID,
+		c.OneDrive.ClientID,
+		c.OneDrive.ClientSecret,
+		nil,
+	)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not authenticate with Azure AD:", err.Error())
+		os.Exit(1)
+	}
+
+	// Grab a token before entering the main loop to flag any authentication
+	// issues early.
+	ctx := context.Background()
+	t, err := cred.GetToken(ctx, shared.TokenRequestOptions{})
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not retrive an Azure AD auth token:", err.Error())
+		os.Exit(1)
+	}
+
+	dur, err := time.ParseDuration(c.BackupInterval)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not parse the backup interval:", err.Error())
+		os.Exit(1)
+	}
+	// TODO: Execute a GET request against the Todoist API before the main loop
+	// to help troubleshoot
+
+	k := time.NewTicker(dur)
+
+	for {
+		select {
+		case <-k.C:
+			if t.ExpiresOn.After(time.Now()) {
+				t, err := cred.GetToken(ctx, shared.TokenRequestOptions{})
+
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Could not retrive an Azure AD auth token:", err.Error())
+					os.Exit(1)
+				}
+			}
+
+			tr1, err := http.NewRequest("GET", todoistBackupURL, nil)
+
+			if err != nil {
+				fmt.Fprintln(
+					os.Stderr,
+					"Unable to generate an HTTP request. URL:",
+					todoistBackupURL,
+					"Reason:",
+					err.Error())
+			}
+
+			tr1.Header.Add("Authorization", "Bearer "+c.TodoistAPIKey)
+			http.DefaultClient.Do(tr1)
+
+			// TODO: Unmarshal the response into an AvailableBackup
+			// TODO: Pick the latest available backup
+
+			// TODO: Grab the latest available backup from Todoist
+			// TODO: Send the payload to Microsoft Graph (grabbed the
+			// token already--just need to build the URL path to send the payload to)
+		case <-g:
+			fmt.Fprintln(os.Stderr, "Received interrupt. Stopping.")
+			os.Exit(0)
+		}
+
+	}
 }
